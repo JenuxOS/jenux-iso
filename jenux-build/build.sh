5a85f10d30d2522b956772502cd91871712430f7
Daniel Nash <dnl.nash@gmail.com>
2025-08-23 20:13:37 -0400

    Build Script: Use smaller block size to reduce squashfs creation time

--- jenux-build/build.sh
+++ jenux-build/build.sh
@@ -219,7 +219,7 @@ mkdir -p "${work_dir}/iso/arch/${arch}"
 fi
 arch-chroot ${script_path}/${work_dir}/${arch}/airootfs /bin/pacman -Q > "${work_dir}/iso/arch/pkglist.${arch}.txt"
 cd ${work_dir}/${arch}/airootfs
-mksquashfs . "${script_path}/${work_dir}/iso/arch/${arch}/airootfs.sfs" -comp xz
+mksquashfs . "${script_path}/${work_dir}/iso/arch/${arch}/airootfs.sfs" -b 16384
     cd "${script_path}/${work_dir}/iso/arch/${arch}"
 sha512sum airootfs.sfs > airootfs.sha512
 cd ${script_path}
